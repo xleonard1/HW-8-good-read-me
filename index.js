2dbbd2667bd1b44e655dc63e84928de50df28d7c
xleonard1 <xavier.lnrd@gmail.com>
2022-05-01 15:35:44 -0400

    user prompts are working

---
+++ index.js
@@ -0,0 +1,76 @@
+const inquirer = require('inquirer')
+const fs = require('fs')
+
+const promptUser = () => {
+    return inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What would you like to name your README?'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:',
+        },
+        {
+            type: 'input',
+            name: 'motivation',
+            message: 'what was your Motivation?'
+
+        },
+        {
+            type: 'input',
+            name: 'why',
+            message: 'why did you build this project?'
+        },
+        {
+            type: 'input',
+            name: 'problems',
+            message: 'What problems does it solve?'
+        },
+        {
+           type: 'input',
+           name: 'Contents',
+           message: 'Add a table of contents to make it easy for users to find what they need'
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'What are the steps required to install your project?'
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Provide instructions and examples for use.'
+        },
+        {
+            type: 'input',
+            name: 'contribution',
+            message: 'Tell others how they should contribute'
+        },
+        {
+            type: 'input',
+            name: 'Tests',
+            message: 'Provide instructions on how to run your tests.'
+        }
+    ])
+}
+
+const chooseLicense = () => {
+    return inquirer.prompt([
+        {
+            type: 'checkbox',
+            name: 'noLicense',
+            choices: ['no license', 'MIT License', 'GNU GPLv3']
+        }
+    ])
+} 
+
+const init = () => {
+    promptUser()
+    chooseLicense
+
+}
+
+init()
